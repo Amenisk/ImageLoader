3bbd67934aa966fc37c8e0dccf3b92d63fbed138
Amenisk <k.ziyatdinov2003@gmail.com>
2022-10-28 16:37:13 +0300

    Changed code

--- ImageLoader/Data/FileSystemService.cs
+++ ImageLoader/Data/FileSystemService.cs
@@ -7,17 +7,18 @@ namespace ImageLoader.Data
 {
     public class FileSystemService
     {
-        public void UploadImageToDb()
+        public void UploadImageToDb(string path)
         {
             var client = new MongoClient("mongodb://localhost");
             var database = client.GetDatabase("Images");
             var gridFS = new GridFSBucket(database);
+            string[] paths = Directory.GetFiles(path);
 
-            for (int i = 1; i < 8; i++)
+            foreach (var p in paths)
             {
-                using (FileStream fs = new FileStream($"C:/Users/LENOVO/Pictures/Images/{i}.jpg", FileMode.Open))
+                using (FileStream fs = new FileStream(p, FileMode.Open))
                 {
-                    gridFS.UploadFromStream($"{i}.jpg", fs);
+                    gridFS.UploadFromStream(GetNameFile(p), fs);
                 }
             }
         }
@@ -43,13 +44,19 @@ namespace ImageLoader.Data
             var names = new List<string>();
             var images = collection.Find(x => x.Filename != null).ToList<GridFSFileInfo>();
 
-
             foreach (var image in images)
             {
                 names.Add(image.Filename);
             }
 
             return names;
+        }
+
+        private string GetNameFile(string path)
+        {
+            string[] partPath = path.Split('\\');
+
+            return partPath[partPath.Length - 1];
         }
     }
 }
